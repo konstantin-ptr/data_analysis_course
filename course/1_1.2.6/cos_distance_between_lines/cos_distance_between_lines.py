71b69bfd454ebe0a7b8f901ee8d70327630da87e
Konstantin Petrov <petrov.constant@gmail.com>
2018-08-22 15:05:50 +0300

    add comment

--- course/1_1.2.6/cos_distance_between_lines/cos_distance_between_lines.py
+++ course/1_1.2.6/cos_distance_between_lines/cos_distance_between_lines.py
@@ -25,12 +25,14 @@ def predict_encoding(file_path, n_lines=20):
 
     return chardet.detect(rawdata)['encoding']
 
+#считываем файл в список
 txt_lists=[]
 with open(file_name, encoding=predict_encoding(file_name)) as f:
     for line in f: 
         word_list=(re.split('[^a-zа-я0-9]',line.lower()))
         txt_lists.append(word_list)
 
+#формируем словарь слов
 t=[]
 text_concur=[]
 for n in range(len(txt_lists)):
@@ -39,14 +41,15 @@ for n in range(len(txt_lists)):
         text_concur.append(t[i])
 words_dict=dict(Counter([i for i in text_concur if len(i)>0]))
 
+#создаем матрицу значений и заполняем ее
 doc_array=np.zeros([len(txt_lists),len(words_dict)],dtype = int)
-
 for i in range(len(txt_lists)):
     n=0
     for k in words_dict.keys():
         doc_array[i,n]=(txt_lists[i].count(k))
         n+=1
 
+#считаем косинусное расстояние по матрице
 v=list(map(lambda x: distance.cosine(doc_array[preddict_line],x),[doc_array[i] for i in range(doc_array.shape[0])]))
 v=pd.DataFrame(v, columns=['Косинусное расстояние'])
 v.sort_values(by='Косинусное расстояние', inplace=True)
